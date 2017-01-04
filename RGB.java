bc1d6e0bf37609b76356ade6e5dda617521adb8e
mishcat <i.mishcat@yahoo.com>
2017-01-04 15:57:08 -0500

    Add files via upload
    Fixed bugs w/ initialization

--- RGB.java
+++ RGB.java
@@ -1,7 +1,8 @@
 package game;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
-
+import gui.GUI;
 
 
 public class RGB {
@@ -19,24 +20,27 @@ public class RGB {
 	}
 	
 	/** Finds red, green, and blue color values at a specific pixel */
-	public RGB Colors(BufferedImage im, int x, int y){
-		int color = im.getRGB(x,y);
-		int r=  (color & 0x00ff0000) >> 16;
-		int g = (color & 0x0000ff00) >> 8;
-		int b=  color & 0x000000ff;
-		RGB a = new RGB(r,g,b);
-		return a;
+	public RGB(Color color){
+		
+		this.red=  color.getRed();
+		this.green = color.getGreen();
+		this.blue =  color.getBlue();
+
 	}
 	
+
+	
 	/**Calculates average red, green, and blue values */
-	public static  RGB average(RGB first, RGB second, RGB third){
+	public RGB(RGB first, RGB second, RGB third){
 		
-		int r=(first.red + second.red + third.red)/3;
-		int g=(first.green + second.green + third.green)/3;
-		int b=(first.blue + second.blue + third.blue)/3;
-		RGB a=new RGB(r,g,b);
-		return a;
+		this.red=(first.red + second.red + third.red)/3;
+		this.green=(first.green + second.green + third.green)/3;
+		this.blue=(first.blue + second.blue + third.blue)/3;
+
 	}
+	
+	//test
+	/*
 	public static void main(String[] args){
 		RGB rgb1= new RGB(1,2,3);
 		RGB rgb2= new RGB(3,2,1);
@@ -45,8 +49,8 @@ public class RGB {
 		System.out.println(a.red);
 		
 	}
-
-}
+*/
+	}
 
 
 
